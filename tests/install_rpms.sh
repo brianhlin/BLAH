138fc57622df0ecc0f663501d43fdd0ee949f0d5
Brian Lin <brianhlin@gmail.com>
2021-04-05 11:25:14 -0500

    Revert "FIXME: see if tests are set up properly"
    This reverts commit ce0f65c67dec2fd16fa2c0ea68441028592b6ff3.

--- tests/install_rpms.sh
+++ tests/install_rpms.sh
@@ -11,8 +11,8 @@ mkdir -p /var/run/lock
 RPM_LOCATION=/tmp/rpmbuild/RPMS/x86_64
 [[ $BUILD_ENV == osg ]] && extra_repos='--enablerepo=osg-upcoming-development'
 
-# package_version=`grep Version blahp/rpm/blahp.spec | awk '{print $2}'`
-# yum localinstall -y $RPM_LOCATION/blahp-${package_version}* $extra_repos
+package_version=`grep Version blahp/rpm/blahp.spec | awk '{print $2}'`
+yum localinstall -y $RPM_LOCATION/blahp-${package_version}* $extra_repos
 
 # Install batch systems that will exercise the blahp in osg-test
 yum install -y osg-ce-condor \
